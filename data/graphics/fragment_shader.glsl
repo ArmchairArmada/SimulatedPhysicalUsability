f6d40073aea52921fabb99cb77e57bf5bb6927ec
Nathan Moore <482946+ArmchairArmada@users.noreply.github.com>
2016-12-29 01:54:10 -0500

    Playing around with shaders a little more

--- data/graphics/fragment_shader.glsl
+++ data/graphics/fragment_shader.glsl
@@ -7,8 +7,16 @@ in vec3 theUv;
 out vec4 outColor;
 
 void main() {
+    float specularPower = 25.0;
+    float specularIntensity = 0.25;
+    vec3 materialColor = vec3(0.1, 0.35, 0.8);
     vec3 lightDirection = normalize(vec3(1.0, 2.0, 3.0));
     float diffuse = max(dot(lightDirection, theNormal), 0.0);
-    float specular = clamp(pow(max(dot(lightDirection, theNormal), 0.0), 25.0)*0.75, 0.0, 1.0);
-    outColor = vec4((theColor + diffuse) * vec3(0.2, 0.5, 0.7) + specular, 1.0);
+    float specular = clamp(pow(max(dot(lightDirection, theNormal), 0.0), specularPower)*specularIntensity, 0.0, 1.0);
+    float ambient = dot(theNormal, vec3(0, 1, 0))/2.0 + 0.5;
+    float rim = clamp((1.0-pow(dot(theNormal, vec3(0, 0, 1))+0.5, 3.0))*0.75, 0.0, 1.0);
+    
+    //outColor = vec4((theColor + diffuse) * materialColor + specular, 1.0);
+    
+    outColor = vec4((theColor + ambient + diffuse) * materialColor + specular + ambient*rim, 1.0);
 }
